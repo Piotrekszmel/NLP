510377839a174a1129e1876787da236e1d9c4502
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-28 16:32:28 +0200

    created funciton for removing accented characters

--- Processing_and_Understanding_Text/app.py
+++ Processing_and_Understanding_Text/app.py
@@ -1,7 +1,18 @@
 from data_retrieval import build_dataset
+from nltk.tokenize.toktok import ToktokTokenizer
+import nltk
+import spacy
+
+nlp = spacy.load('en_core', parse=True, tag=True, entity=True)
+
+tokenizer = ToktokTokenizer()
+stopwords_list = nltk.corpus.stopwords.words('english')
+stopwords_list.remove('no')
+stopwords_list.remove('not')
 
 seed_urls = ['https://inshorts.com/en/read/technology',
              'https://inshorts.com/en/read/sports',
              'https://inshorts.com/en/read/world']
 
 news_df = build_dataset(seed_urls)
+
--- Processing_and_Understanding_Text/text_preprocessing.py
+++ Processing_and_Understanding_Text/text_preprocessing.py
@@ -14,4 +14,9 @@ def strip_html_tags(text):
     stripped_text = soup.get_text()
     return stripped_text
 
-print(strip_html_tags('<html><h2>Some important text</h2></html>'))
+
+def remove_accented_chars(text):
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return text
+
+
