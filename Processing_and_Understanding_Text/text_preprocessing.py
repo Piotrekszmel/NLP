f0761d88e0e71c817572790ba6c860f2b295e552
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-28 16:41:58 +0200

    created function for removing special characters

--- Processing_and_Understanding_Text/text_preprocessing.py
+++ Processing_and_Understanding_Text/text_preprocessing.py
@@ -25,3 +25,11 @@ def expand_contractions(text):
     return text
 
 
+def remove_special_characters(text, remove_digits=False):
+    pattern = r'[^a-zA-Z0-9\s]' if not remove_digits else r'[^a-zA-Z\s]'
+    text = re.sub(pattern, '', text)
+    return text
+
+
+print(remove_special_characters("Well this was fun! What do you think? 123#@!", 
+                          remove_digits=True))
