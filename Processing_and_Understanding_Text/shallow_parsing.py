ddf0ae2cf0dc58900e10a39123ab7364b8bc0868
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-08-29 10:55:48 +0200

    added NGramTagChunker function

--- Processing_and_Understanding_Text/shallow_parsing.py
+++ Processing_and_Understanding_Text/shallow_parsing.py
@@ -1,12 +1,45 @@
 from nltk.corpus import conll2000
-from nltk.chunk.util import tree2conllstr, conlltags2tree
+from nltk.chunk.util import tree2conlltags, conlltags2tree
+from nltk.tag import UnigramTagger, BigramTagger
+from nltk.chunk import ChunkParserI
+
+def conll_tag_chunks(chunk_sents):
+    tagged_sents = [tree2conlltags(tree) for tree in chunk_sents]
+    return [[(t, c) for (w, t, c) in sent] for sent in tagged_sents]
+
+
+def combined_tagger(train_data, taggers, backoff=None):
+    for tagger in taggers:
+        backoff = tagger(train_data, backoff=backoff)
+    return backoff
+
+
+
+
+class NGramTagChunker(ChunkParserI):
+    
+    def __init__(self, train_sentences, tagger_classes=[UnigramTagger, BigramTagger]):
+        train_sent_tags = conll_tag_chunks(train_sentences)
+        self.chunk_tagger = combined_tagger(train_sent_tags, tagger_classes)
+
+
+    def parse(self, tagged_sentence):
+        if not tagged_sentence: 
+            return None
+        pos_tags = [tag for word, tag in tagged_sentence]
+        chunk_pos_tags = self.chunk_tagger.tag(pos_tags)
+        chunk_tags = [chunk_tag for (pos_tag, chunk_tag) in chunk_pos_tags]
+        wpc_tags = [(word, pos_tag, chunk_tag) for ((word, pos_tag), chunk_tag)
+                        in zip(tagged_sentence, chunk_tags)]
+        return conlltags2tree(wpc_tags)
+
 
 data = conll2000.chunked_sents()
 
 train_data = data[:10900]
 test_data = data[10900:]
 
-wtc = tree2conllstr(train_data[1])
-print(wtc)
-
-tree = conlltags2tree(wtc)
+ntc = NGramTagChunker(train_data)
+print(ntc.evaluate(test_data))
+#wtc = tree2conlltags(train_data[1])
+#tree = conlltags2tree(wtc) 
